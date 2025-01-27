11cbb143611634ee72c3e4aa7786a374edc765bc
Akash <akashdixit2301@gmail.com>
2025-01-27 16:56:53 +0530

    exploring the GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 
 const app = express();
+ 
+//this will only handle to GET call /user/
+app.get("/user" ,(req , res) =>{
+       res.send({firstNmae : "AKash" , lastName: "Dixit "})
+})
 
-app.use("/hello" ,(req ,res )=>{
-       res.send("hello page ");
-       })
-       
 
+app.post("/user" , (req , res) =>{
+       // console.log("save data to datbase");
+       //saving data to DB
+       res.send("data sucessfully to database")
+})
+
+
+
+//this will match all the HTTP method API calls to /test
 app.use("/test" ,(req ,res )=>{
 res.send("hello from the server");
 })
+
+
 
 
  
